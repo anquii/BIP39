862ec59862a62ef647ac2fa680a3b3391d3b53a2
anquii <50277099+anquii@users.noreply.github.com>
2022-02-04 12:58:41 +0000

    Updated Package.swift to include CryptoSwift as a dependency

--- Package.swift
+++ Package.swift
@@ -4,19 +4,18 @@ import PackageDescription
 
 let package = Package(
     name: "BIP39",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13)
+    ],
     products: [
-        .library(
-            name: "BIP39",
-            targets: ["BIP39"]
-        ),
+        .library(name: "BIP39", targets: ["BIP39"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", .upToNextMajor(from: "1.4.2"))
     ],
     targets: [
-        .target(
-            name: "BIP39"
-        ),
-        .testTarget(
-            name: "BIP39Tests",
-            dependencies: ["BIP39"]
-        ),
+        .target(name: "BIP39", dependencies: ["CryptoSwift"]),
+        .testTarget(name: "BIP39Tests", dependencies: ["BIP39"])
     ]
 )
