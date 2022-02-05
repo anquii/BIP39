6f7b8b6edcc091a275ca162986a1b72fcbd5e1ea
anquii <50277099+anquii@users.noreply.github.com>
2022-02-05 22:14:52 +0000

    Updated EntropyGeneratorTests

--- Tests/BIP39Tests/EntropyGeneratorTests.swift
+++ Tests/BIP39Tests/EntropyGeneratorTests.swift
@@ -5,4 +5,34 @@ final class EntropyGeneratorTests: XCTestCase {
     private func sut() -> EntropyGenerator {
         .init()
     }
+
+    func testGivenWeakestEntropySecurity_WhenGenerateEntropy_AndCountBytes_ThenEqual16() throws {
+        let security = EntropySecurity.weakest
+        let entropy = try sut().entropy(security: security)
+        XCTAssertEqual(entropy.count, 16)
+    }
+
+    func testGivenWeakEntropySecurity_WhenGenerateEntropy_AndCountBytes_ThenEqual20() throws {
+        let security = EntropySecurity.`weak`
+        let entropy = try sut().entropy(security: security)
+        XCTAssertEqual(entropy.count, 20)
+    }
+
+    func testGivenMediumEntropySecurity_WhenGenerateEntropy_AndCountBytes_ThenEqual24() throws {
+        let security = EntropySecurity.medium
+        let entropy = try sut().entropy(security: security)
+        XCTAssertEqual(entropy.count, 24)
+    }
+
+    func testGivenStrongEntropySecurity_WhenGenerateEntropy_AndCountBytes_ThenEqual28() throws {
+        let security = EntropySecurity.strong
+        let entropy = try sut().entropy(security: security)
+        XCTAssertEqual(entropy.count, 28)
+    }
+
+    func testGivenStrongestEntropySecurity_WhenGenerateEntropy_AndCountBytes_ThenEqual32() throws {
+        let security = EntropySecurity.strongest
+        let entropy = try sut().entropy(security: security)
+        XCTAssertEqual(entropy.count, 32)
+    }
 }
