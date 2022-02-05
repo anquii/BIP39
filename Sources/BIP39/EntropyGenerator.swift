90b2f80fc05e3c41f8968f5a81707087012f6af8
anquii <50277099+anquii@users.noreply.github.com>
2022-02-05 22:05:35 +0000

    Updated EntropyGenerator

--- Sources/BIP39/EntropyGenerator.swift
+++ Sources/BIP39/EntropyGenerator.swift
@@ -5,9 +5,11 @@ public protocol EntropyGenerating {
 }
 
 public struct EntropyGenerator: EntropyGenerating {
+    private static let bitsInByte = 8
+
     public func entropy(security: EntropySecurity) throws -> Data {
         let entropyLength = Int(entropySecurity: security)
-        var bytes = [UInt8](repeating: 0, count: entropyLength / 8)
+        var bytes = [UInt8](repeating: 0, count: entropyLength / Self.bitsInByte)
 
         let status = SecRandomCopyBytes(kSecRandomDefault, bytes.count, &bytes)
         if status == errSecSuccess {
