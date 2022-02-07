d295b25fd6a04c55dcf1add7a4825d45dc035ff4
anquii <50277099+anquii@users.noreply.github.com>
2022-02-07 13:13:46 +0000

    Added MnemonicConstructor.swift

--- Sources/BIP39/MnemonicConstructor.swift
+++ Sources/BIP39/MnemonicConstructor.swift
@@ -5,6 +5,10 @@ public protocol MnemonicConstructing {
 }
 
 public struct MnemonicConstructor {
+    private static let bitsInByte = 8
+    private static let entropyEncodingMultipleInBits = 32
+    private static let bitSegments = 11
+
     private let wordListProvider: WordListProviding
 
     public init(wordListProvider: WordListProviding) {
@@ -15,6 +19,25 @@ public struct MnemonicConstructor {
 // MARK: - MnemonicConstructing
 extension MnemonicConstructor: MnemonicConstructing {
     public func mnemonic(entropy: Data) -> String {
-        ""
+        let hexEncodedPrefix = String(repeating: "0", count: Self.bitsInByte)
+        let entropyBits = entropy.flatMap { (hexEncodedPrefix + String($0, radix: 2)).suffix(Self.bitsInByte) }
+        let hashBits = entropy.sha256().flatMap { (hexEncodedPrefix + String($0, radix: 2)).suffix(Self.bitsInByte) }
+
+        let entropyLength = entropy.count * Self.bitsInByte
+        let checksumLength = entropyLength / Self.entropyEncodingMultipleInBits
+        let checksumBits = hashBits.prefix(checksumLength)
+        let concatenatedBits = entropyBits + checksumBits
+
+        var words = [String]()
+        let wordList = wordListProvider.wordList
+
+        for i in 0..<(concatenatedBits.count / Self.bitSegments) {
+            let startIndex = concatenatedBits.index(concatenatedBits.startIndex, offsetBy: i * Self.bitSegments)
+            let endIndex = concatenatedBits.index(startIndex, offsetBy: Self.bitSegments)
+            let wordIndex = Int(strtoul(String(concatenatedBits[startIndex..<endIndex]), nil, 2))
+            words.append(wordList[wordIndex])
+        }
+
+        return words.joined(separator: " ")
     }
 }
