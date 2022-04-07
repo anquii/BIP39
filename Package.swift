a3dfc61f9a67d9e7d153ca5e683d06f428d4858a
anquii <50277099+anquii@users.noreply.github.com>
2022-04-07 20:24:55 +0100

    Updated Package.swift

--- Package.swift
+++ Package.swift
@@ -14,15 +14,16 @@ let package = Package(
             targets: ["BIP39"]
         )
     ],
+    dependencies: [
+        .package(
+            url: "https://github.com/anquii/CryptoSwiftWrapper.git",
+            from: "1.4.3"
+        )
+    ],
     targets: [
-        .binaryTarget(
-            name: "CryptoSwift",
-            url: "https://github.com/krzyzanowskim/CryptoSwift/releases/download/1.4.3/CryptoSwift.xcframework.zip",
-            checksum: "5eaa8fe30805789ad5472edb27182d41afecf1283f0c6efb0afc9261b28f5cf6"
-        ),
         .target(
             name: "BIP39",
-            dependencies: ["CryptoSwift"]
+            dependencies: ["CryptoSwiftWrapper"]
         ),
         .testTarget(
             name: "BIP39Tests",
