7e6f5d5c91cb4b05fd6b07a32028fa19f1f8006f
anquii <50277099+anquii@users.noreply.github.com>
2023-01-18 11:25:47 +0000

    Updated SeedDerivator

--- Sources/BIP39/SeedDerivator.swift
+++ Sources/BIP39/SeedDerivator.swift
@@ -17,10 +17,22 @@ public struct SeedDerivator {
 // MARK: - SeedDerivating
 extension SeedDerivator: SeedDerivating {
     public func seed(mnemonic: String, passphrase: String = "") throws -> Data {
+        guard
+            let password = mnemonic
+                .decomposedStringWithCompatibilityMapping
+                .data(using: .utf8)?
+                .bytes,
+            let salt = (Self.saltConstant + passphrase)
+                .decomposedStringWithCompatibilityMapping
+                .data(using: .utf8)?
+                .bytes
+        else {
+            throw SeedDerivatorError.invalidInput
+        }
         do {
             let keyDerivation = try PKCS5.PBKDF2(
-                password: password(mnemonic: mnemonic),
-                salt: salt(passphrase: passphrase),
+                password: password,
+                salt: salt,
                 iterations: Self.iterations,
                 keyLength: Self.keyLength,
                 variant: Self.variant
@@ -31,16 +43,5 @@ extension SeedDerivator: SeedDerivating {
         } catch let error as PKCS5.PBKDF2.Error {
             throw SeedDerivatorError(error)
         }
-    }
-}
-
-// MARK: - Helpers
-fileprivate extension SeedDerivator {
-    func password(mnemonic: String) -> Array<UInt8> {
-        mnemonic.bytes
-    }
-
-    func salt(passphrase: String) -> Array<UInt8> {
-        (Self.saltConstant + passphrase).bytes
     }
 }
